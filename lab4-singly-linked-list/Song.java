26d0cc8b2de12a77f19b7a442c6baef1603682aa
npthuy162 <npthuy162@gmail.com>
2026-09-18 21:40:52 -0700

    Add Song

---
+++ lab4-singly-linked-list/Song.java
@@ -0,0 +1,25 @@
+package com.mycompany.playlistmanager;
+
+public class Song {
+
+    private final String title;
+    private final String artist;
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    @Override
+    public String toString() {
+        return title + " - " + artist;
+    }
+}
